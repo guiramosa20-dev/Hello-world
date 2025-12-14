951f04027711d083f2520914c2ed870358196409
guiramosa20-dev <guiramosa20@gmail.com>
2025-12-14 18:18:21 -0300

    Funcionalidades finais
    Código pronto para entrar em operação

--- Trabalho_materiais_construção/modulos.py
+++ Trabalho_materiais_construção/modulos.py
@@ -1,6 +1,8 @@
 import json
 from dataclasses import asdict, replace
 from registros import *
+from getpass import getpass
+from time import sleep
 
 #/////////////////////////// Funções de manipulação de dados ///////////////////////////////
 #salvamento de dados em .json:
@@ -33,12 +35,18 @@ def alteraDados(ARQUIVO,index,registro,novoValor):
     if ARQUIVO == 'funcionarios.json':
         for i in range(len(original)):
             if i == index:
-                original[i] = replace(original[i], **{registro: novoValor})
+                try:
+                    original[i] = replace(original[i], **{registro: novoValor})
+                except TypeError:
+                    return
     
     else:
         for i in range(len(original)):
             if i == index:
-                original[i] = replace(original[i], **{registro: novoValor})
+                try:
+                    original[i] = replace(original[i], **{registro: float(novoValor) if registro == 'preco' else int(novoValor) if registro == 'quantidade' else novoValor})
+                except TypeError:
+                    return
     
     salvarDados(ARQUIVO,original)
 
@@ -190,17 +198,205 @@ def addMercadoria():#Onde a variávelestoque = MERCADORIA()
 
 #excluir mercadoria do estoque
 def removeMercadoria():
-    with open('estoque.json','r', encoding='utf-8') as arquivo:
-        data = json.load(arquivo) #dados carregados na memória RAM
-        try:
-            codigo = int(input('Código da mercadoria a ser descartada: ').strip())
-        except ValueError:
-            codigo = 0
-        for i in range(len(data)):
-            if data[i]['codigo'] == codigo:
-                del data[i]
-                print('\033[32mMercadoria removida com sucesso!\033[m')
-                break
+    data = carregarDados('estoque.json')
+    try:
+        codigo = int(input('Código da mercadoria a ser descartada: ').strip())
+    except ValueError:
+        codigo = 0
+    for i in range(len(data)):
+        if data[i].codigo == codigo:
+            del data[i]
+            salvarDados('estoque.json',data)
+            print('\033[32mMercadoria removida com sucesso!\033[m')
+            break
         else:
             print('\033[31mMercadoria não encontrada!\033[m')
 
+def relatorioVendas():
+    try:
+        with open('estoque.json','r', encoding='utf-8') as arquivo:
+            estoque= json.load(arquivo)
+    except FileNotFoundError:
+        estoque = []
+    except json.JSONDecodeError:
+        estoque = []
+
+    estoque.sort(key=lambda x: x['vendas'], reverse=True)
+    print('-'*40)
+    print(f'{"Relatório de Vendas":^40}')
+    print('-'*40)
+    print(f'{"Código":<10}{"Nome":<15}{"Vendas":>15}')
+    print('-'*40)
+    for i in range(len(estoque)):
+        print(f'{estoque[i]["codigo"]:<10}{estoque[i]["nome"]:<15}{estoque[i]["vendas"]:>15}')
+    print('-'*40)
+
+def relatorioEstoque():
+    try:
+        with open('estoque.json','r', encoding='utf-8') as arquivo:
+            estoque= json.load(arquivo)
+    except FileNotFoundError:
+        estoque = []
+    except json.JSONDecodeError:
+        estoque = []
+
+    print('-'*80)
+    print(f'{"Relatório de Estoque":^80}')
+    print('-'*80)
+    print(f'{"Código":<10}{"Nome":<20}{"Descricao":<20}{"Quantidade":<15}{"Preço (R$)":>15}')
+    print('-'*80)
+    for i in range(len(estoque)):
+        print(f'{estoque[i]["codigo"]:<10}{estoque[i]["nome"]:<20}{estoque[i]["descricao"]:<20}{estoque[i]["quantidade"]:<15}{estoque[i]["preco"]:>15.2f}')
+    print('-'*80)
+
+def alteraProduto():
+    try:
+        with open('estoque.json','r', encoding='utf-8') as arquivo:
+            estoque= json.load(arquivo)
+    except FileNotFoundError:
+        estoque = []
+    except json.JSONDecodeError:
+        estoque = []
+
+    codigo = int(input('Código do produto a ter a descrição alterada: ').strip())
+    print()
+    print('-'*20)
+    print('Dados atuais do produto:')
+    print('-'*20)
+    for i in range (len(estoque)):
+        if estoque[i]['codigo'] == codigo:
+            print(f'Código: {estoque[i]["codigo"]}')
+            print(f'Nome: {estoque[i]["nome"]}')
+            print(f'Descrição: {estoque[i]["descricao"]}')
+            print(f'Quantidade: {estoque[i]["quantidade"]}')
+            print(f'Preço: R$ {estoque[i]["preco"]:.2f}')
+            print('-'*20)
+
+            chave = ''
+            print('qual dado deseja alterar?\n1 - Nome\n2 - Descrição\n3 - Quantidade\n4 - Preço\n5 - Cancelar')
+
+            data = int(input('...').strip())
+
+            match data:
+                case 1:
+                    chave = 'nome'
+                case 2:
+                    chave = 'descricao'
+                case 3:
+                    chave = 'quantidade'
+                case 4:
+                    chave = 'preco'
+                case 5:
+                    return
+                case _:
+                    print('\033[31mOpção inválida!\033[m')
+
+            novaDescricao = input('Nova descrição: ').strip()
+            alteraDados('estoque.json',i,chave,novaDescricao)
+            print('\033[32mDescrição alterada com sucesso!\033[m')
+            break
+    else:
+        print('\033[31mProduto não encontrado!\033[m')
+    
+def alteraFuncionario():
+    try:
+        with open('funcionarios.json','r', encoding='utf-8') as arquivo:
+            efetivo= json.load(arquivo)
+    except FileNotFoundError:
+        efetivo = []
+    except json.JSONDecodeError:
+        efetivo = []
+
+    nome = input('Nome do funcionário a ter a descrição alterada: ').strip().title()
+    print()
+    print('-'*20)
+    print('Dados atuais do funcionário:')
+    print('-'*20)
+    for i in range (len(efetivo)):
+        if efetivo[i]['nome'] == nome:
+            print(f'Código: {efetivo[i]["codigo"]}')
+            print(f'Nome: {efetivo[i]["nome"]}')
+            print(f'Senha: {efetivo[i]["senha"]}')
+            print('-'*20)
+
+            print('qual dado deseja alterar?\n1 - Código\n2 - Nome\n3 - Senha\n4 - Cancelar')
+            data = int(input('...').strip())
+
+            match data:
+                case 1:
+                    chave = 'codigo'
+                case 2:
+                    chave = 'nome'
+                case 3:
+                    chave = 'senha'
+                case 4:
+                    return
+                case _:
+                    print('\033[31mOpção inválida!\033[m')
+
+            novaDescricao = input('Nova descrição: ').strip()
+            alteraDados('funcionarios.json',i,chave,novaDescricao)
+            print('\033[32mDescrição alterada com sucesso!\033[m')
+            break
+    else:
+        print('\033[31mFuncionário não encontrado!\033[m')
+    
+def removeFuncionario():
+    data = carregarDados('funcionarios.json')
+        
+    try:
+        nome = input('Nome do funcionário a ser removido: ').strip().title()
+    except ValueError:
+        nome = ''
+    for i in range(len(data)):
+        if data[i].nome == nome:
+            del data[i]
+            salvarDados('funcionarios.json',data)
+            print('\033[32mFuncionário removido com sucesso!\033[m')
+            break
+    else:
+        print('\033[31mFuncionário não encontrado!\033[m')
+
+#/////////////////////////// Função de login //////////////////////////////////
+def login():
+    # Cabeçalho de inicialização do código:
+    print('-'*20)
+    print(''*5,'Casa do Prego')
+    print('-'*20)
+    print('-')
+
+    while True:
+        # Gerenciamento do acesso do funcionário:
+        print('Escolha:\n 1 - Fazer Login \n 2 - Cadastrar funcionário \n 3 - Sair')
+        try:
+            escolha = int(input('...').strip())
+        except ValueError:
+            escolha = 0
+
+        match escolha:
+            case 1:#cadastro de 1 funcionário
+                print('-'*20)
+                print('Faça seu login:')
+                try:
+                    codigo = int(input('Código: ').strip())
+                except ValueError:
+                    print('\033[31mPor favor, insira um número válido para o código.\033[m')
+                    continue
+                nome = input('Nome: ').strip().title()
+                senha = getpass('Senha: ')
+
+                if checarFuncionario(codigo,nome,senha):#Verificando se o funcionário está cadastrado
+                    return codigo
+                else:
+                    print('\033[31mFuncionário não cadastrado!\033[m')
+    
+            case 2:#Login
+               addFuncionario()
+
+            case 3:#Sair
+                print('Saindo...')
+                sleep(3)
+                return 3
+                
+            case _:#Informa que a opção escolhida é inválida
+                print('\033[31mOpção inválida!\033[m')
--- Trabalho_materiais_construção/principal.py
+++ Trabalho_materiais_construção/principal.py
@@ -1,56 +1,7 @@
 from unittest import case
 from registros import *
 from modulos import *
-from getpass import getpass
-from time import sleep
 
-#/////////////////////////// Função de login //////////////////////////////////
-def login():
-    # Cabeçalho de inicialização do código:
-    print('-'*20)
-    print(''*5,'Casa do Prego')
-    print('-'*20)
-    print('-')
-
-    while True:
-        # Gerenciamento do acesso do funcionário:
-        print('Escolha:\n 1 - Fazer Login \n 2 - Cadastrar funcionário \n 3 - Sair')
-        try:
-            escolha = int(input('...').strip())
-        except ValueError:
-            escolha = 0
-
-        match escolha:
-            case 1:#cadastro de 1 funcionário
-                print('-'*20)
-                print('Faça seu login:')
-                try:
-                    codigo = int(input('Código: ').strip())
-                except ValueError:
-                    print('\033[31mPor favor, insira um número válido para o código.\033[m')
-                    continue
-                nome = input('Nome: ').strip().title()
-                senha = getpass('Senha: ')
-
-                if checarFuncionario(codigo,nome,senha):#Verificando se o funcionário está cadastrado
-                    return codigo
-                else:
-                    print('\033[31mFuncionário não cadastrado!\033[m')
-    
-            case 2:#Login
-               addFuncionario()
-
-            case 3:#Sair
-                print('Saindo...')
-                sleep(3)
-                return 3
-                
-            case _:#Informa que a opção escolhida é inválida
-                print('\033[31mOpção inválida!\033[m')
-
-#/////////////////////////// Main code //////////////////////////////////
-
-#Daqui pra baixo os prints são apenas placeholders para as funcionalidades que vão ser implementadas
 while True:
     escolha2 = login()
     if escolha2 == 3:
@@ -100,12 +51,39 @@ while True:
 
         case 300:#Gerenciamento administrativo
             while True:
-                print('Relatório de vendas')
-                print('Relatório de estoque')
-                print('Mudar descrição de produto')
-                print('Nodar descrição de funcionário')
-                print('Excluir produto')
-                print('Excluir funcionário')
+                print('-'*40)
+                print('Gerenciamento administrativo:')
+                print('-'*40)
+                print('1 - Relatório de vendas')
+                print('2 - Relatório de estoque')
+                print('3 - Mudar dados de um produto')
+                print('4 - Mudar dados de um funcionário')
+                print('5 - Excluir produto')
+                print('6 - Excluir funcionário')
+                print('7 - Sair')
+                try:
+                    escolha2 = int(input('...').strip())
+                except ValueError:
+                    escolha2 = 0
+                
+                match escolha2:
+                    case 1:
+                        relatorioVendas()
+                    case 2:
+                        relatorioEstoque()
+                    case 3:
+                        alteraProduto()
+                    case 4:
+                        alteraFuncionario()
+                    case 5:
+                        removeMercadoria()
+                    case 6:
+                        removeFuncionario()
+                    case 7:
+                        break
+                    case _:
+                        print('\033[31mOpção inválida!\033[m')
+
         case _:
             print('\033[31mOpção inválida!\033[m')
 
